6dd1b95b1abe77ab17e576ded5a13edd077a9fd5
HERAUX-BOCQUET Jean-Claude <72662725+Lapitos@users.noreply.github.com>
2021-03-22 10:04:40 +0100

    Version 0.21
    Ajout de commentaires

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
+// Contient les fonctions qui s'appliquent aux différentes routes pour les utilisateurs
+
 const express = require('express'); // plugin express
+const bodyParser = require('body-parser'); // Plugin Body Parser
 const router = express.Router(); // router fourni par express
 const userCtrl = require('../controllers/user'); // gestion de l'utilisateur
-const verifyPassword = require('../middleware/verifyPassword');
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
